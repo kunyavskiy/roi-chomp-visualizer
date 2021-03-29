f1f4075899c83a2ffdab3abf6f236eb2f086e302
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2021-03-29 22:15:28 +0300

    handle more participant errors

--- src/main/kotlin/GameManager.kt
+++ src/main/kotlin/GameManager.kt
@@ -5,8 +5,10 @@ import java.lang.StringBuilder
 import kotlin.random.Random
 
 import com.sun.jna.Platform;
+import kotlinx.coroutines.sync.withLock
 import java.io.*
 import java.lang.Exception
+import java.lang.NumberFormatException
 
 fun pipePrefix() : String {
     if (Platform.isWindows()) {
@@ -14,6 +16,10 @@ fun pipePrefix() : String {
     } else {
         return "/tmp/"
     }
+}
+
+class BadMoveException(message: String?) : Exception(message) {
+
 }
 
 class GameManager(
@@ -76,11 +82,11 @@ class GameManager(
                         games += 1
                         firstMove = !firstMove
                         runBlocking {
-                            drawMutex.lock()
-                            for (i in 0 until fieldSize) {
-                                columnHeights[i].value = fieldSize
+                            drawMutex.withLock {
+                                for (i in 0 until fieldSize) {
+                                    columnHeights[i].value = fieldSize
+                                }
                             }
-                            drawMutex.unlock()
                         }
                     }
                     val builder = StringBuilder()
@@ -90,17 +96,23 @@ class GameManager(
                     gameLog.value = builder.toString()
                 } catch (e : Exception) {
                     gameError.value = e.message ?: "Неизвестная ошибка"
+                } finally {
+                    input?.close()
+                    output?.close()
                 }
             }
         }
     }
 
     suspend fun processMove(x: Int, y: Int) {
-        drawMutex.lock()
-        for (i in x until fieldSize) {
-            columnHeights[i].value = minOf(columnHeights[i].value, y)
+        drawMutex.withLock {
+            if (columnHeights[x].value <= y) {
+                throw BadMoveException("Невалидный ход: клетка ${x+1} ${y+1} уже закрашена")
+            }
+            for (i in x until fieldSize) {
+                columnHeights[i].value = minOf(columnHeights[i].value, y)
+            }
         }
-        drawMutex.unlock()
         gameLogArray.add(Pair(x + 1, y + 1))
     }
 
@@ -118,7 +130,7 @@ class GameManager(
         var any = false
         while (columnHeights[0].value > 0) {
             if (move) {
-                val line = input.readLine() ?: throw RuntimeException("Неожиданный конец ввода")
+                val line = input.readLine() ?: throw BadMoveException("Неожиданный конец вывода решения")
                 if (!any && line.isInt() && line.toInt() == 0) {
                     if (!firstMove) {
                         gameLogArray.removeLast()
@@ -126,8 +138,20 @@ class GameManager(
                     return false;
                 }
                 any = true
-                val (x, y) = line.split(" ").map { it.toInt() }
-                processMove(x - 1, y - 1)
+                try {
+                    val lineSplited = line.split(" ")
+                    if (lineSplited.size != 2) {
+                        throw NumberFormatException()
+                    }
+                    val (x, y) = lineSplited.map { it.toInt() }
+                    if (x in 1..fieldSize && y in 1..fieldSize) {
+                        processMove(x - 1, y - 1)
+                    } else {
+                        throw BadMoveException("Ожидалось два числа от 1 до ${fieldSize}, а решение вывело $x $y")
+                    }
+                } catch (e : NumberFormatException) {
+                    throw BadMoveException("Ожидалось два числа, а решение вывело \"${line}\"")
+                }
             } else {
                 val randomMove =
                     (0 until fieldSize).flatMap { lhs -> (0 until fieldSize).map { Pair(lhs, it) } }.filter {
